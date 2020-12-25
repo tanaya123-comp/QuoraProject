f9e0adec0dcf6cb1476cfd89a1a12df8fe2922f1
tanaya123 <tanayawankar58@gmail.com>
2020-12-25 20:06:23 +0530

    Added Templates and static directory

--- QuoraApp/urls.py
+++ QuoraApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import HomePage
 
 urlpatterns = [
-
+    path('homepage',HomePage),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
